27ca34b5cef5244b2aa7490b5cb10feb6a6499d2
Andy Clark <ajc97@cantab.net>
2019-10-14 22:06:26 +0100

    Updates

---
+++ precache-manifest.ebd03a25bab420a4206a054d6bd1bb14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7336ce87dbf6bfbd2fe169207c6c842f",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "ca831c6f7284b2d48e89",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b8ec6e18b246f2355700",
+    "url": "/robofriends/static/css/main.68d4d6ee.chunk.css"
+  },
+  {
+    "revision": "ca831c6f7284b2d48e89",
+    "url": "/robofriends/static/js/2.31d9b7d8.chunk.js"
+  },
+  {
+    "revision": "b8ec6e18b246f2355700",
+    "url": "/robofriends/static/js/main.6b3450d6.chunk.js"
+  },
+  {
+    "revision": "eb54e97a82f44e946317",
+    "url": "/robofriends/static/js/runtime-main.7aa1f784.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
